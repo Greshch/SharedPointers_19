2663cb3e35c1b7651f18fd75fd22864a31de946b
Greshch <greshchook@gmail.com>
2022-09-07 09:06:59 +0300

    It was completed

--- SharedPtr.h
+++ SharedPtr.h
@@ -1,1 +1,104 @@
 #pragma once
+
+#include <iostream>
+#include <vector>
+
+template<typename T>
+struct Pair {
+	T adress;
+	int links = 0;
+};
+
+template<typename T>
+class SharedPtr {
+public:
+
+	SharedPtr() = default;
+
+	SharedPtr(T* t) :
+		m_ptr(t)
+	{
+		Add(t);
+	}
+
+	SharedPtr(SharedPtr const& other) :
+		m_ptr(other.m_ptr)
+	{
+		int id = GetId(other.m_ptr);
+		IncrLinkById(id);
+	}
+
+	SharedPtr& operator= (SharedPtr const& other) {
+		if (this != &other) {
+			int old_id = GetId(m_ptr);
+			delete m_ptr;
+			CleanById(old_id);
+			
+			m_ptr = other.m_ptr;
+			int new_id = GetId(other.m_ptr);
+			IncrLinkById(new_id);
+		}
+		return *this;
+	}
+
+	T* Get() {
+		if (m_ptr == nullptr) {
+			throw std::runtime_error("It wasn't initialaized");
+		}
+		return m_ptr;
+	}
+
+	T& operator *() {
+		return *Get();
+	}
+
+	T* operator->() {
+		return Get();
+	}
+
+	~SharedPtr() {
+		int id = GetId(m_ptr);
+		DecLinkById(id);
+		int links = buffer[id].links;
+		if (links == 0) {
+			delete m_ptr;
+			m_ptr = nullptr;
+			CleanById(id);
+			std::cout << "Destr-r completed\n";
+		}
+	}
+
+private:
+	T* m_ptr = nullptr;
+	static std::vector<Pair<T*>> buffer;
+
+	static const int notFounded = -1;
+
+	static int GetId(T* pT) {
+		for (size_t i = 0; i < buffer.size(); i++) {
+			if (buffer[i].adress == pT) {
+				return i;
+			}
+		}
+		return notFounded;
+	}
+
+	static void Add(T* pT) {
+		buffer.push_back({pT, 1});
+	}
+
+	static void IncrLinkById(int id) {
+		buffer[id].links++;
+	}
+
+	static void DecLinkById(int id) {
+		buffer[id].links--;
+	}
+
+	static void CleanById(int id) {
+		buffer.erase(buffer.begin() + id);
+	}
+};
+
+template<typename T>
+std::vector<Pair<T*>> SharedPtr<T>::buffer {};
--- main.cpp
+++ main.cpp
@@ -1,4 +1,16 @@
+#include <iostream>
+#include "SharedPtr.h"
+
 int main()
 {
+	// Test 1 for copy ct-r
+	/*SharedPtr<int> sh_1(new int);
+	SharedPtr<int> sh_2(sh_1);
+	SharedPtr<int> sh_3(new int);*/
 
+	// Test 2 for operator=
+	SharedPtr<int> sh_1(new int);
+	SharedPtr<int> sh_2(new int);
+	sh_2 = sh_1;
+	return 0;
 }
