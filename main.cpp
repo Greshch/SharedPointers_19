e835f719ff5440df127b5f5ffe48232443dfc32e
Greshch <greshchook@gmail.com>
2022-09-10 13:54:22 +0300

    It maybe done

--- main.cpp
+++ main.cpp
@@ -4,26 +4,23 @@
 int main()
 {
 	// Test 1 for copy ct-r
-	/*SharedPtr<int> sh_1(new int);
+	/*SharedPtr<int> sh_1(new int{ 1 });
 	SharedPtr<int> sh_2(sh_1);
-	SharedPtr<int> sh_3(new int);*/
+	SharedPtr<int> sh_3(new int{ 3 });*/
 
 	// Test 2 for operator=
-	/*SharedPtr<int> sh_1(new int);
-	SharedPtr<int> sh_2(new int);
+	/*SharedPtr<int> sh_1(new int{ 1 });
+	SharedPtr<int> sh_2(new int{2});
 	sh_2 = sh_1;*/
 
-	/*int* i = new int;
-	*i = 123;
+	
+	// Test 3
+	/*SharedPtr<int> iPt(new int{ 123 });
+	SharedPtr<double> dPt(new double{ 321.67 });*/
 
-	double* d = new double;
-	*d = 321.67;
-	SharedPtr<int> iPt(i);
-	SharedPtr<double> dPt(d);*/
-
+	// Test 4
 	SharedPtr<int> iPt1(new int{ 10 });
 	SharedPtr<int> iPt1Copy(iPt1);
-
 	SharedPtr<int> iPt2(new int{ 20 });
 	iPt2 = iPt1;
 	return 0;
