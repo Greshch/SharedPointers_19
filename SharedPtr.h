9ea8064b8c5c8e238b6d853d13e8d9201a66a402
Greshch <greshchook@gmail.com>
2022-09-14 07:01:14 +0300

    Not static implementation

--- SharedPtr.h
+++ SharedPtr.h
@@ -1,108 +1,77 @@
 #pragma once
 
-#include <iostream>
-#include <vector>
-
-template<typename T>
-struct Pair {
-	T adress;
-	int links = 0;
-};
-
-template<typename T>
-class SharedPtr {
+template<class T>
+class SharedPtr
+{
 public:
+    SharedPtr(T* ptr) :
+        m_ptr(ptr),
+        m_links(new int{1})
+    {}
 
-	SharedPtr() = default;
+    SharedPtr(SharedPtr const& obj)
+    {
+        Update(obj);
+    }
 
-	SharedPtr(T* t) :
-		m_ptr(t)
-	{
-		Add(t);
-	}
+    SharedPtr& operator= (SharedPtr const& obj)
+    {
+        CleanUp();
+        Update(obj);
+        return *this;
+    }
 
-	SharedPtr(SharedPtr const& other) :
-		m_ptr(other.m_ptr)
-	{
-		int id = GetId(other.m_ptr);
-		IncrLinkById(id);
-	}
+    void Update(const SharedPtr &obj) {
+        m_ptr = obj.m_ptr;
+        m_links = obj.m_links;
+        ++(*obj.m_links);
+    }
 
-	SharedPtr& operator= (SharedPtr const& other) {
-		if (this != &other) {
-			int old_id = GetId(m_ptr);
-			CleanUp();
-			m_ptr = other.m_ptr;
-			int new_id = GetId(other.m_ptr);
-			IncrLinkById(new_id);
-		}
-		return *this;
-	}
+    T* Get() const
+    {
+        return m_ptr;
+    }
 
-	T* Get() {
-		if (m_ptr == nullptr) {
-			throw std::runtime_error("It wasn't initialaized");
-		}
-		return m_ptr;
-	}
+    T* operator->()
+    {
+        return Get();
+    }
 
-	T& operator *() {
-		return *Get();
-	}
+    T& operator*()
+    {
+        return *Get();
+    }
 
-	T* operator->() {
-		return Get();
-	}
+    operator bool ()
+    {
+        return  m_ptr != nullptr;
+    }
 
-	~SharedPtr() {
-		CleanUp();
-	}
-
-	void CleanUp()
-	{
-		std::cout << "#CleanUp\t" << *m_ptr << "\n";
-		int id = GetId(m_ptr);
-		DecLinkById(id);
-		int links = buffer[id].links;
-		if (links == 0) {
-			std::cout << "\t!!Destr-r completed!!\t" << *m_ptr << "\n";
-			delete m_ptr;
-			m_ptr = nullptr;
-			CleanById(id);
-		}
-	}
+    ~SharedPtr()
+    {
+        CleanUp();
+    }
 
 private:
-	T* m_ptr = nullptr;
-	static std::vector<Pair<T*>> buffer;
+    void CleanUp()
+    {
+        --(*m_links);
+        if (*m_links == 0)
+        {
+            Free();
+        }
+    }
 
-	static const int notFounded = -1;
+    void Free()
+    {
+        delete m_ptr;
+        m_ptr = nullptr;
 
-	static int GetId(T* pT) {
-		for (size_t i = 0; i < buffer.size(); i++) {
-			if (buffer[i].adress == pT) {
-				return i;
-			}
-		}
-		return notFounded;
-	}
+        delete m_links;
+        m_links = nullptr;
+    }
 
-	static void Add(T* pT) {
-		buffer.push_back({pT, 1});
-	}
-
-	static void IncrLinkById(int id) {
-		buffer[id].links++;
-	}
-
-	static void DecLinkById(int id) {
-		buffer[id].links--;
-	}
-
-	static void CleanById(int id) {
-		buffer.erase(buffer.begin() + id);
-	}
+private:
+    T* m_ptr = nullptr;
+    int* m_links = nullptr;
 };
-
-template<typename T>
-std::vector<Pair<T*>> SharedPtr<T>::buffer {};
